1d0996efca0d4055e46a0231ac609f3e3149e604
Denis Duliçi <denisdulici@gmail.com>
2021-10-22 11:53:40 +0300

    fixed tailwind presenter

--- src/Presenters/Admin/Tailwind.php
+++ src/Presenters/Admin/Tailwind.php
@@ -3,6 +3,7 @@
 namespace Akaunting\Menu\Presenters\Admin;
 
 use Akaunting\Menu\Presenters\Presenter;
+use Illuminate\Support\Str;
 
 class Tailwind extends Presenter
 {
@@ -11,7 +12,11 @@ class Tailwind extends Presenter
      */
     public function getOpenTagWrapper()
     {
-        return PHP_EOL . '<ul class="flex flex-col justify-center">' . PHP_EOL;
+        return PHP_EOL . '<div class="navbar-inner">
+            <!-- Collapse -->
+            <div class="collapse" id="sidenav-collapse-main">
+                <!-- Nav items -->
+                <ul class="flex flex-col justify-center">' . PHP_EOL;
     }
 
     /**
@@ -19,7 +24,9 @@ class Tailwind extends Presenter
      */
     public function getCloseTagWrapper()
     {
-        return PHP_EOL . '</ul>' . PHP_EOL;
+        return PHP_EOL . '</ul>
+            </div>
+        </div>' . PHP_EOL;
     }
 
     /**
@@ -27,15 +34,27 @@ class Tailwind extends Presenter
      */
     public function getMenuWithoutDropdownWrapper($item)
     {
-        return '<li' . $this->getActiveState($item) . '><a href="' . $item->getUrl() . '" ' . $item->getAttributes() . '>' . $item->getIcon() . ' <span>' . $item->title . '</span></a></li>' . PHP_EOL;
+        $iconState = $this->iconState($item);
+
+        $html = '<li class="group relative mb-2.5">';
+        $html .= '  <a class="flex items-center text-secondary" href="' . $item->getUrl() . '" ' . $item->getAttributes() . '>';
+        $html .= '  <div class="w-8 h-8 flex items-center justify-center">';
+        $html .=      $item->getIcon($iconState);
+        $html .= '   </div>';
+        $html .= '    <span class="text-sm ml-2 hover:font-bold">' . $item->title . '</span>';
+        $html .= '    <span class="bg-secondary absolute h-5 -right-5 rounded-tl-lg rounded-bl-lg opacity-0 group-hover:opacity-100 transition-all" style="width: 5px;" ></span> ';
+        $html .= '  </a>';
+        $html .= '</li>' . PHP_EOL;
+
+        return $html;
     }
 
     /**
      * {@inheritdoc }.
      */
-    public function getActiveState($item, $state = ' class="active"')
+    public function getActiveState($item, $state = '' )
     {
-        return $item->isActive() ? $state : null;
+        return $item->isActive() ? $state : '-outline';
     }
 
     /**
@@ -46,9 +65,32 @@ class Tailwind extends Presenter
      *
      * @return null|string
      */
-    public function getActiveStateOnChild($item, $state = 'active')
+    public function getActiveStateOnChild($item, $state = 'open')
     {
-        return $item->hasActiveOnChild() ? $state : null;
+        return $item->hasActiveOnChild() ? $state : '';
+    }
+
+    public function chevronState($item, $state = '-up')
+    {
+        return $item->hasActiveOnChild() ? $state : '-down';
+    }
+
+    public function iconState($item, $state = '')
+    {
+        return $item->hasActiveOnChild() ? $state : '-outline';
+    }
+
+    /**
+     * Get active state on child items.
+     *
+     * @param $item
+     * @param string $state
+     *
+     * @return null|string
+     */
+    public function getShowStateOnChild($item, $state = 'open')
+    {
+        return $item->hasActiveOnChild() ? $state : ' ';
     }
 
     /**
@@ -56,7 +98,7 @@ class Tailwind extends Presenter
      */
     public function getDividerWrapper()
     {
-        return '<li class="divider"></li>';
+        return '<hr class="my-3">';
     }
 
     /**
@@ -64,7 +106,7 @@ class Tailwind extends Presenter
      */
     public function getHeaderWrapper($item)
     {
-        return '<li class="header">' . $item->title . '</li>';
+        return '<h6 class="navbar-heading p-0 text-muted">' . $item->title . '</h6>';
     }
 
     /**
@@ -72,17 +114,24 @@ class Tailwind extends Presenter
      */
     public function getMenuWithDropDownWrapper($item)
     {
-        return '<li class="treeview' . $this->getActiveStateOnChild($item, ' active') . '">
-		          <a href="#">
-					' . $item->getIcon() . ' <span>' . $item->title . '</span>
-                    <span class="pull-right-container">
-                      <i class="fa fa-angle-left pull-right"></i>
-                    </span>
-			      </a>
-			      <ul class="treeview-menu">
-			      	' . $this->getChildMenuItems($item) . '
-			      </ul>
-		      	</li>'
+        $id = Str::slug($item->title);
+
+        $iconState = $this->iconState($item);
+
+        return '
+        <details '. $this->getActiveStateOnChild($item) .'>
+            <summary class="relative mb-2.5 flex mt-2 items-center cursor-pointer text-secondary" href="#navbar-' . $id . '">
+                    ' . $item->getIcon($iconState) . '
+                <span class="text-sm font-normal ml-2">' . $item->title . '</span>
+                <ion-icon data-id="cash" name="chevron'. $this->chevronState($item) .'" class="absolute right-0"></ion-icon>
+            </summary>
+            <div class="mt-2 ml-8 " id="navbar-' . $id . '">
+                <ul class="relative pb-2.5">
+                    ' . $this->getChildMenuItems($item) . '
+                </ul>
+
+            </div>
+        </details>'
         . PHP_EOL;
     }
 
@@ -95,17 +144,19 @@ class Tailwind extends Presenter
      */
     public function getMultiLevelDropdownWrapper($item)
     {
-        return '<li class="treeview' . $this->getActiveStateOnChild($item, ' active') . '">
-		          <a href="#">
-					' . $item->getIcon() . ' <span>' . $item->title . '</span>
-			      	<span class="pull-right-container">
-                      <i class="fa fa-angle-left pull-right"></i>
-                    </span>
-			      </a>
-			      <ul class="treeview-menu">
-			      	' . $this->getChildMenuItems($item) . '
-			      </ul>
-		      	</li>'
+        $id = Str::slug($item->title);
+
+        return '<li class="nav-item">
+    <a class="nav-link' . $this->getActiveState($item) . '" href="#navbar-' . $id . '" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="navbar-' . $id . '">
+        ' . $item->getIcon() . '
+        <span class="nav-link-text">' . $item->title . '</span>
+    </a>
+    <div class="collapse' . $this->getShowStateOnChild($item) . '" id="navbar-' . $id . '">
+        <ul class="nav nav-sm flex-column">
+            ' . $this->getChildMenuItems($item) . '
+        </ul>
+    </div>
+</li>'
         . PHP_EOL;
     }
 }
